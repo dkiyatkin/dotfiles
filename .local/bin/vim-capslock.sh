051d84e973b73d335b81685ca7c20e3dcbae6162
Dmitriy Kiyatkin <info@dkiyatkin.com>
2016-03-13 03:31:21 +0400

    nvim konsole

--- .local/bin/vim-capslock.sh
+++ .local/bin/vim-capslock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # для всех приложений xkb-switch -n, для vim <F2>
 # если gvim отправлять через xvkbd, то в нем может появляться предупреждение о изменении файла
+# Установить xkb-switch, xdotool, xkb-switch
 W=`xdotool getactivewindow`
 S1=`xprop -id ${W} | awk '/WM_CLASS/{print $4}'`
 # sleep 0.1 # TODO правильно ловить окно, у xvkbd нормально без этого
@@ -14,8 +15,9 @@ if [ $S1 = '"Gvim"' ]; then
 # как вариант: попробовать менять title через tmux
 elif [ $S1 = '"Konsole"' ]; then
   S2=`xprop -id ${W} | awk '/WM_NAME/{for (i = 3; i <= NF; i++) printf $i " "; print ""}' | sed -n "1p"`
-  if [[ $S2 == *" : vim — Konsole"* ]]; then
+  if [[ $S2 == *"vim — Konsole"* ]]; then
     # sleep 0.1 ; xdotool key --window $W --clearmodifiers 0x5c F6
+    xkb-switch -n # переключаем обратно
     xvkbd -xsendevent -text '\[F2]'
   else
     # xkb-switch -n
