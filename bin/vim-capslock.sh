49f4eaace7ef3123c6fbf6f3c829c8b35a07d60e
Dmitriy Kiyatkin <info@dkiyatkin.com>
2016-06-20 01:55:50 +0400

    vim auto change keyboard layout

--- bin/vim-capslock.sh
+++ bin/vim-capslock.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
+
 # для всех приложений xkb-switch -n, для vim <F2>
 # если gvim отправлять через xvkbd, то в нем может появляться предупреждение о изменении файла
-# Установить xkb-switch, xdotool, xkb-switch
-W=`xdotool getactivewindow`
-S1=`xprop -id ${W} | awk '/WM_CLASS/{print $4}'`
-# sleep 0.1 # TODO правильно ловить окно, у xvkbd нормально без этого
-if [ $S1 = '"Gvim"' ]; then
-  xkb-switch -n
-  # sleep 0.1 ; xdotool key --window $W --clearmodifiers 0x5c F6
-  xvkbd -xsendevent -text '\[F2]'
+# Установить xkb-switch, xkb-switch
+
 # TODO
 # фильтровать другие терминалы с запущенным vim
-# не определяется yakuake, konsole-tmux, konsole-ssh
+# не определяется konsole-tmux, konsole-ssh
 # как вариант: попробовать менять title через tmux
-elif [ $S1 = '"Konsole"' ]; then
-  S2=`xprop -id ${W} | awk '/WM_NAME/{for (i = 3; i <= NF; i++) printf $i " "; print ""}' | sed -n "1p"`
-  if [[ $S2 == *"vim — Konsole"* ]]; then
-    # sleep 0.1 ; xdotool key --window $W --clearmodifiers 0x5c F6
-    xkb-switch -n # переключаем обратно
-    xvkbd -xsendevent -text '\[F2]'
-  else
-    # xkb-switch -n
-    :
-  fi
-else
-  # xkb-switch -n
-  :
-fi
 
+function _vimSwitch {
+  xkb-switch -n # переключаем обратно
+  xvkbd -xsendevent -text '\[F2]'
+}
+
+function vimSwitch {
+  TITLE=$1
+  if [[ ( $TITLE == *": nvim"* ) || ( $TITLE == *": vim"* ) ]]; then
+    _vimSwitch
+  fi
+}
+
+# W=`xdotool getactivewindow`
+W=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
+S1=`xprop -id ${W} WM_CLASS`
+if [[ $S1 == *"\"gvim\", \"Gvim\""* ]]; then
+  _vimSwitch
+elif [[ $S1 == *"\"konsole\", \"konsole\""* ]]; then
+  TITLE=$(xprop -id ${W} WM_NAME)
+  vimSwitch "$TITLE"
+elif [[ $S1 == *"\"yakuake\", \"yakuake\""* ]]; then
+  TITLE=$(qdbus org.kde.yakuake /yakuake/tabs tabTitle $(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeSessionId))
+  vimSwitch "$TITLE"
+fi
